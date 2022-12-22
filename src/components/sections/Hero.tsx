2a3b0b8e7f6d7654dc07ac05b5a4e070fcc06b75
mtvzee <pgzv01@gmail.com>
2022-12-22 17:36:27 +0900

    Heroの雛形

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,8 +1,83 @@
+import { motion, Variants } from 'framer-motion';
+import { BsArrowRightShort } from 'react-icons/bs';
+
+const textContainer: Variants = {
+  hidden: { x: -500, opacity: 0, translateY: '-50%' },
+  show: { x: 0, opacity: 1, transition: { duration: 1 } },
+};
+const linkContainer: Variants = {
+  // hidden: { opacity: 0 },
+  // show: {
+  //   opacity: 1,
+  //   transition: {
+  //     staggerChildren: 0.5,
+  //   },
+  // },
+  // hidden: { opacity: 0 },
+  // show: { opacity: 1, transition: { duration: 1 } },
+};
+const item: Variants = {
+  hidden: { x: -100, opacity: 0 },
+  show: { x: 0, opacity: 1 },
+};
+
 const Hero = () => {
   return (
-    <div className="h-screen">
-      <h1>Takuya</h1>
+    <div className="container">
+      <motion.div
+        className="absolute top-1/2  left-0"
+        variants={textContainer}
+        initial="hidden"
+        whileInView="show"
+      >
+        <h1 className="text-8xl">Takuya Matsutani</h1>
+        <span className="text-5xl">Portfolio</span>
+      </motion.div>
 
+      <ul className="absolute top-1/2 right-0 -translate-y-1/2 flex flex-col space-y-4">
+        <li>
+          <motion.a
+            href="#about"
+            className="hero-a"
+            variants={linkContainer}
+            initial="hidden"
+            animate="show"
+          >
+            <span>About Me</span>
+            <motion.button variants={item}>
+              <BsArrowRightShort className="w-8 h-8" />
+            </motion.button>
+          </motion.a>
+        </li>
+        <li>
+          <motion.a
+            href="#projects"
+            className="hero-a"
+            variants={linkContainer}
+            initial="hidden"
+            animate="show"
+          >
+            <span>Projects</span>
+            <motion.button variants={item}>
+              <BsArrowRightShort className="w-8 h-8" />
+            </motion.button>
+          </motion.a>
+        </li>
+        <li>
+          <motion.a
+            href="#contact"
+            className="hero-a"
+            variants={linkContainer}
+            initial="hidden"
+            animate="show"
+          >
+            <span>Contact</span>
+            <motion.button variants={item} initial="hidden" whileHover="show">
+              <BsArrowRightShort className="w-8 h-8" />
+            </motion.button>
+          </motion.a>
+        </li>
+      </ul>
     </div>
   );
 };
