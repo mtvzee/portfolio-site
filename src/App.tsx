9146ccae233d5c28a845fcf1b8e97438d0a9e390
mtvzee <pgzv01@gmail.com>
2022-12-26 20:02:12 +0900

    ページレイアウトの変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,35 @@
 import { Keyboard, Mousewheel } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import About from './components/sections/About';
-import Contact from './components/sections/Contact';
 import Hero from './components/sections/Hero';
-import Projects from './components/sections/Projects';
+import Header from './components/Header';
+import Project from './components/Project';
+import { projectData } from './data/projectData';
 
 const App = () => {
   return (
-    <Swiper
-      tag="main"
-      direction={'vertical'}
-      slidesPerView={1}
-      speed={1000}
-      mousewheel={true}
-      keyboard={true}
-      modules={[Mousewheel, Keyboard]}
-      className="h-screen "
-    >
-      <SwiperSlide id="hero" tag="section" className="h-screen">
-        <Hero />
-      </SwiperSlide>
-      <SwiperSlide id="about" tag="section" className="h-screen">
-        <About />
-      </SwiperSlide>
-      <SwiperSlide id="projects" tag="section" className="h-screen">
-        <Projects />
-      </SwiperSlide>
-      <SwiperSlide id="contact" tag="section" className="h-screen">
-        <Contact />
-      </SwiperSlide>
-    </Swiper>
+    <>
+      <Header />
+      <Swiper
+        tag="main"
+        direction={'vertical'}
+        slidesPerView={1}
+        speed={1000}
+        mousewheel={true}
+        keyboard={true}
+        modules={[Mousewheel, Keyboard]}
+        className="h-screen "
+      >
+        <SwiperSlide id="hero" tag="section" className="h-screen">
+          <Hero />
+        </SwiperSlide>
+        {projectData.map((data) => (
+          <SwiperSlide key={data.id} tag="section" className="h-screen">
+            <Project id={data.id} title={data.title} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
   );
 };
 
