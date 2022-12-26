d30ec7610bd4477bf364b22b742a3d7d8b769a9f
mtvzee <pgzv01@gmail.com>
2022-12-26 20:00:15 +0900

    ポートフォリオの各データを外部ファイルからインポートする

---
+++ src/data/projectData.ts
@@ -0,0 +1,5 @@
+export const projectData = [
+  { id: 1, title: 'Chat App', thumbnail: '', gitHubURL: '', siteURL: '' },
+  { id: 2, title: 'Dashboard', thumbnail: '', gitHubURL: '', siteURL: '' },
+  { id: 3, title: 'Netflix Clone', thumbnail: '', gitHubURL: '', siteURL: '' },
+];
