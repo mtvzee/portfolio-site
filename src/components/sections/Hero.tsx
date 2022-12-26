f217d669ebe57053bce2365cbd3cafe5d4e34478
mtvzee <pgzv01@gmail.com>
2022-12-26 20:02:44 +0900

    ナビゲーション機能の削除

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,5 +1,4 @@
 import { motion, Variants } from 'framer-motion';
-import { BsArrowRightShort } from 'react-icons/bs';
 
 const textContainer: Variants = {
   hidden: { x: -500, opacity: 0, translateY: '-50%' },
@@ -33,51 +32,6 @@ const Hero = () => {
         <h1 className="text-8xl">Takuya Matsutani</h1>
         <span className="text-5xl">Portfolio</span>
       </motion.div>
-
-      <ul className="absolute top-1/2 right-0 -translate-y-1/2 flex flex-col space-y-4">
-        <li>
-          <motion.a
-            href="#about"
-            className="hero-a"
-            variants={linkContainer}
-            initial="hidden"
-            animate="show"
-          >
-            <span>About Me</span>
-            <motion.button variants={item}>
-              <BsArrowRightShort className="w-8 h-8" />
-            </motion.button>
-          </motion.a>
-        </li>
-        <li>
-          <motion.a
-            href="#projects"
-            className="hero-a"
-            variants={linkContainer}
-            initial="hidden"
-            animate="show"
-          >
-            <span>Projects</span>
-            <motion.button variants={item}>
-              <BsArrowRightShort className="w-8 h-8" />
-            </motion.button>
-          </motion.a>
-        </li>
-        <li>
-          <motion.a
-            href="#contact"
-            className="hero-a"
-            variants={linkContainer}
-            initial="hidden"
-            animate="show"
-          >
-            <span>Contact</span>
-            <motion.button variants={item} initial="hidden" whileHover="show">
-              <BsArrowRightShort className="w-8 h-8" />
-            </motion.button>
-          </motion.a>
-        </li>
-      </ul>
     </div>
   );
 };
